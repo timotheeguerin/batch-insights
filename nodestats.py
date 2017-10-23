b26f722131b87762177d0956ed6ea7f661c160df
Timothee Guerin <tiguerin@microsoft.com>
2017-10-23 16:40:25 -0700

    try upload

--- nodestats.py
+++ nodestats.py
@@ -15,6 +15,19 @@ import psutil
 import time
 from applicationinsights import TelemetryClient
 
+def setup_logger():
+    # logger defines
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s.%(msecs)03dZ %(levelname)s %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+logger = setup_logger()
+
 
 # global defines
 _IS_PLATFORM_WINDOWS = platform.system() == 'Windows'
@@ -292,19 +305,6 @@ class NodeStatsUtils:
                 delta += nanoseconds // 100
         return delta
 
-
-def setup_logger():
-    # logger defines
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s.%(msecs)03dZ %(levelname)s %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
-
-logger = setup_logger()
 
 # global defines
 _DEFAULT_STATS_UPDATE_INTERVAL = 5
