7b0bf85c886c795ff74bfb64d21876a09f52a2b7
Timothee Guerin <tiguerin@microsoft.com>
2017-10-23 16:39:06 -0700

    try upload

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
 apt-get install python-pip
 pip install psutil python-dateutil applicationinsights
-wget https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
+wget --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
 python nodestats.py
