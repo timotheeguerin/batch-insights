b84e0d7de1fcaa4a0036a761bc48f7c96febec33
Timothee Guerin <tiguerin@microsoft.com>
2017-10-23 14:35:49 -0700

    change

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
 sudo apt-get install python-pip;
 pip install psutil
-wget https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.python
+wget https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
 python nodestats.py
