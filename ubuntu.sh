2fdbba6b9acaeef77ba4412d7cb6357654984541
Timothee Guerin <tiguerin@microsoft.com>
2017-10-25 16:20:35 -0700

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,6 @@
 set -e
 
+apt-get update
 apt-get -y install python-pip
 pip install psutil python-dateutil applicationinsights
 wget --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
