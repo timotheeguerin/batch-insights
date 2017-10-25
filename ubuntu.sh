d16cf3a8857c336cc55cbce4b587c29023735b9d
Timothee Guerin <tiguerin@microsoft.com>
2017-10-25 16:17:01 -0700

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,4 +3,4 @@ set -e
 apt-get -y install python-pip
 pip install psutil python-dateutil applicationinsights
 wget --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
-python nodestats.py &
+python nodestats.py > node-stats.log &
