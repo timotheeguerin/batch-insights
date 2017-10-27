4a470f703b54c7b80f70ab8616fb32eda54e8495
Timothee Guerin <tiguerin@microsoft.com>
2017-10-27 14:51:41 -0700

    INstall pip dep

---
+++ centos.sh
@@ -0,0 +1,13 @@
+set -e
+yum -y install gcc 
+
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+echo "Python version:"
+python --version
+echo "Pip version:"
+pip --version
+pip install psutil python-dateutil applicationinsights
+
+wget --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
+python nodestats.py > node-stats.log 2>&1 &
