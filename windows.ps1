f37ae4947b23997e73d8699d9a023b08acfe0334
Timothee Guerin <tiguerin@microsoft.com>
2017-11-16 14:50:22 -0800

    yes

--- windows.ps1
+++ windows.ps1
@@ -1,6 +1,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y python
+Write-Host "Python version:"
 python --version
 pip install psutil python-dateutil applicationinsights
-Invoke-WebRequest --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py
+Write-Host "Downloading nodestats.py"
+Invoke-WebRequest --no-cache https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
 start-job { python nodestats.py > node-stats.log 2>&1 }
