d09572f49834cae68c0e88e0a6b7435e42ce534f
Timothee Guerin <tiguerin@microsoft.com>
2017-11-16 15:07:28 -0800

    yes

--- windows.ps1
+++ windows.ps1
@@ -5,4 +5,4 @@ python --version
 pip install psutil python-dateutil applicationinsights
 Write-Host "Downloading nodestats.py"
 Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
-start-job { python nodestats.py > node-stats.log 2>&1 }
+Start-Process python -ArgumentList nodestats.py -RedirectStandardOutput node-stats.log -RedirectStandardError node-stats.log
