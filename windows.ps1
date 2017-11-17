16fc11a05ed8390f1970842119954eba63e11d64
Timothee Guerin <tiguerin@microsoft.com>
2017-11-17 11:30:30 -0800

    Windows finally works

--- windows.ps1
+++ windows.ps1
@@ -1,9 +1,9 @@
-# Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# choco install -y python
-# Write-Host "Python version:"
-# python --version
-# pip install psutil python-dateutil applicationinsights
-# Write-Host "Downloading nodestats.py"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y python
+Write-Host "Python version:"
+python --version
+pip install psutil python-dateutil applicationinsights
+Write-Host "Downloading nodestats.py"
 Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
 Write-Host "Starting App insights background process in $env:AZ_BATCH_TASK_WORKING_DIR"
 
