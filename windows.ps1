267de0c683d0d562eaa0d8e137c4960682849cf0
Timothee Guerin <tiguerin@microsoft.com>
2017-11-16 16:45:11 -0800

    fix

--- windows.ps1
+++ windows.ps1
@@ -7,4 +7,4 @@ Write-Host "Downloading nodestats.py"
 Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
 Write-Host "Starting background process in $env.AZ_BATCH_TASK_WORKING_DIR"
 
-Start-Process cmd -ArgumentList -NoNewWindow '\C .\nodestats.py *>> .\node-stats.log'
+Start-Process cmd  -NoNewWindow -ArgumentList '\C .\nodestats.py *>> .\node-stats.log'
