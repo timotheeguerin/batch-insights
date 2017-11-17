ec3fabe0c5b754d6f01b2304a6dfa4f2716e1558
Timothee Guerin <tiguerin@microsoft.com>
2017-11-17 10:06:24 -0800

    Windows

--- windows.ps1
+++ windows.ps1
@@ -5,15 +5,15 @@
 # pip install psutil python-dateutil applicationinsights
 # Write-Host "Downloading nodestats.py"
 Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
-Write-Host "Starting background process in $env.AZ_BATCH_TASK_WORKING_DIR"
+Write-Host "Starting background process in $env:AZ_BATCH_TASK_WORKING_DIR"
 
-Start-Process python -ArgumentList .\nodestats.py  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -WindowStyle Hidden -Credential 'NT AUTHORITY\\SYSTEM'
+# Start-Process python -ArgumentList .\nodestats.py  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -WindowStyle Hidden -Credential 'NT AUTHORITY\\SYSTEM'
 # start cmd /C "python .\nodestats.py > .\node-stats.log 2>&1"
 
 # python .\nodestats.py *>> .\node-stats.log
-
-# $action = New-ScheduledTaskAction -WorkingDirectory $env:AZ_BATCH_TASK_WORKING_DIR -Execute 'Powershell.exe' -Argument \\\"Get-Counter -Counter {string.Join(",", counters)} -SampleInterval 2 -Continuous | Export-Counter -Path '{resultsFile}' -Force -FileFormat 'BLG'\\\" ; 
-# $principal = New-ScheduledTaskPrincipal -UserID 'NT AUTHORITY\\SYSTEM' -LogonType ServiceAccount -RunLevel Highest ; 
-# Register-ScheduledTask -Action $action -Principal $principal -TaskName '{taskName}' -Force ; 
-# Start-ScheduledTask -TaskName '{taskName}'; 
-# Get-ScheduledTask -TaskName '{taskName}';
+$taskName = "batch-app-insights";
+$action = New-ScheduledTaskAction -WorkingDirectory $env:AZ_BATCH_TASK_WORKING_DIR -Execute 'Powershell.exe' -Argument "python .\nodestats.py > .\node-stats.log 2>&1" ; 
+$principal = New-ScheduledTaskPrincipal -UserID 'NT AUTHORITY\\SYSTEM' -LogonType ServiceAccount -RunLevel Highest ; 
+Register-ScheduledTask -Action $action -Principal $principal -TaskName $taskName -Force ; 
+Start-ScheduledTask -TaskName $taskName; 
+Get-ScheduledTask -TaskName $taskName;
