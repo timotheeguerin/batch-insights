a0a7efd1c688a63a9b3d6db6d35afdcef01e5005
Timothee Guerin <tiguerin@microsoft.com>
2017-11-17 09:22:51 -0800

    Hidden window

--- windows.ps1
+++ windows.ps1
@@ -7,7 +7,7 @@
 Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
 Write-Host "Starting background process in $env.AZ_BATCH_TASK_WORKING_DIR"
 
-Start-Process python -ArgumentList .\nodestats.py  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -WindowStyle Hidden
-# start /B
+# Start-Process python -ArgumentList .\nodestats.py  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -WindowStyle Hidden
+start /B python .\nodestats.py > .\node-stats.log 2>&1
 
 # python .\nodestats.py *>> .\node-stats.log
