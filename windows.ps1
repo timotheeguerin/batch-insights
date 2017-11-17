4931162ec605c4cb02fbd98e8b38e8ec500c80c1
Timothee Guerin <tiguerin@microsoft.com>
2017-11-16 17:37:51 -0800

    Simple

--- windows.ps1
+++ windows.ps1
@@ -4,7 +4,7 @@
 # python --version
 # pip install psutil python-dateutil applicationinsights
 # Write-Host "Downloading nodestats.py"
-# Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
-# Write-Host "Starting background process in $env.AZ_BATCH_TASK_WORKING_DIR"
+Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
+Write-Host "Starting background process in $env.AZ_BATCH_TASK_WORKING_DIR"
 
 Start-Process python -ArgumentList .\nodestats.py -NoNewWindow  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log
