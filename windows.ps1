ce9a3fad45fcef71c7ea4380fd6d2f2146a5736c
Timothee Guerin <tiguerin@microsoft.com>
2017-11-17 10:14:12 -0800

    Windows

--- windows.ps1
+++ windows.ps1
@@ -7,7 +7,7 @@
 Invoke-WebRequest https://raw.githubusercontent.com/timotheeguerin/batch-insights/master/nodestats.py -OutFile nodestats.py
 Write-Host "Starting background process in $env:AZ_BATCH_TASK_WORKING_DIR"
 
-# Start-Process python -ArgumentList .\nodestats.py  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -WindowStyle Hidden -Credential 'NT AUTHORITY\\SYSTEM'
+# Start-Process python -ArgumentList .\nodestats.py  -RedirectStandardOutput .\node-stats.log -RedirectStandardError .\node-stats.err.log -WindowStyle Hidden -Credential 'NT AUTHORITY\SYSTEM'
 # start cmd /C "python .\nodestats.py > .\node-stats.log 2>&1"
 
 # python .\nodestats.py *>> .\node-stats.log
